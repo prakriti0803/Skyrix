05d67bdebe50120d7c1e95d9fdd9916219e356c0
jainhardik06 <2023pietcshardik060@poornima.org>
2025-09-06 18:43:34 +0530

    couples section added

--- src/component/Landing/Main.js
+++ src/component/Landing/Main.js
@@ -1051,6 +1051,42 @@ const Main = () => {
           </div>
         </div>
 
+        {/* Romantic Couple Celebrations Section (after Anniversary Decoration) */}
+        <div className="BirthdayDecorationArea couple-section">
+          <div className="container-fluid">
+            <div className="section-title">
+              <h2>Romantic Couple Celebrations</h2>
+            </div>
+
+            <div className="row g-4 couple-grid">
+              {[
+                { id: 1, label: 'Romantic Dates', img: require('../../assets/images/newimages/couplesection/1.avif') },
+                { id: 2, label: 'Proposal', img: require('../../assets/images/newimages/couplesection/2.avif') },
+                { id: 3, label: 'First Night', img: require('../../assets/images/newimages/couplesection/3.avif') },
+                { id: 4, label: 'Anniversary Decoration', img: require('../../assets/images/newimages/couplesection/4.avif') },
+              ].map((item) => (
+                <div key={item.id} className="col-6 col-lg-3">
+                  <div
+                    className="couple-card"
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => handleCategory({ categoryName: 'ANNIVERSARY' })}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter' || e.key === ' ') {
+                        e.preventDefault();
+                        handleCategory({ categoryName: 'ANNIVERSARY' });
+                      }
+                    }}
+                  >
+                    <img src={item.img} alt={item.label} loading="lazy" />
+                    
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
         <div className="BirthdayDecorationArea" style={{ cursor: "pointer" }}>
           {/* <Slider {...settings1}> */}
           {getDealBannerList?.data?.length > 0
